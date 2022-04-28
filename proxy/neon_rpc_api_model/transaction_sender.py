a006f8919f74f64d736050a3642810b0d7a3b6f5
Neel Somani <neel@berkeley.edu>
2022-04-28 15:39:12 -0500

    Initial commit

--- proxy/neon_rpc_api_model/transaction_sender.py
+++ proxy/neon_rpc_api_model/transaction_sender.py
@@ -318,7 +318,7 @@ class SimpleNeonTxStrategy(BaseNeonTxStrategy, abc.ABC):
     def execute(self) -> (NeonTxResultInfo, [str]):
         signer = self.s.resource.signer
         tx_list = self.s.build_account_tx_list(self._skip_create_account)
-        if len(tx_list) > 0:
+        if tx_list:
             SolTxListSender(self.s, tx_list, self.s.account_txs_name).send(signer)
             self.s.done_account_tx_list(self._skip_create_account)
 
